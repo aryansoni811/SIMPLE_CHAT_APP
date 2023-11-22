30c718cee6c3fb691d7ef8f1fcf85a3daf2dc800
Aryan Soni <arpitsoni811@gmail.com>
2023-11-22 13:59:28 +0530

    added dbs

--- index.js
+++ index.js
@@ -1,37 +1,78 @@
-
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const mongoose = require("mongoose");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+mongoose.connect(
+  "mongodb+srv://arpitsoni811:mern_chat1234@cluster0.gcrpcuk.mongodb.net/?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB");
 });
 
+const messageSchema = new mongoose.Schema({
+  username: String,
+  message: String,
+  timestamp: { type: Date, default: Date.now },
+});
 
-io.on('connection', (socket) => {
-  console.log('A user connected');
+const Message = mongoose.model("Message", messageSchema);
 
-  socket.on('user joined', (username) => {
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("user joined", (username) => {
     socket.username = username;
-    io.emit('user joined', username);
+    io.emit("user joined", username);
   });
 
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+  socket.on("chat message", (msg) => {
+    const message = {
+      username: socket.username,
+      message: msg.message,
+    };
+
+    const newMessage = new Message(message);
+
+    // Use the promise returned by save
+    newMessage
+      .save()
+      .then((savedMessage) => {
+        console.log("Message saved to MongoDB:", savedMessage);
+      })
+      .catch((error) => {
+        console.error("Error saving message to MongoDB:", error);
+      });
+
+    const msg_string = JSON.stringify(msg["message"]).slice(1, -1);
+    console.log(`message sent is ${msg_string}`);
+    if (msg_string.startsWith("/")) {
+      socket.emit("chat message", msg);
+    } else {
+      io.emit("chat message", message);
+    }
   });
 
-  socket.on('disconnect', () => {
+  socket.on("disconnect", () => {
     if (socket.username) {
       console.log(`User ${socket.username} disconnected`);
-      io.emit('user left', socket.username);
+      io.emit("user left", socket.username);
     }
   });
 });
 
 server.listen(4000, () => {
-  console.log('Server listening on *:4000');
+  console.log("Server listening on *:4000");
 });
